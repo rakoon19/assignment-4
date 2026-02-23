679a66144cc9c4ccca038dd0f0f1ed14d74a8ff8
rakoon19 <rahatakondo18@gmail.com>
2026-02-23 15:51:46 +0600

    added btn

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ let scoreofTotal = getbyID('scoreofTotal');
 let scoreofInterview = getbyID('scoreofInterview');
 let scoreofRejected = getbyID('scoreofRejected');
 let availablejobsCount = getbyID('availablejobsCount')
+
+// accessing btns
+let btnofAll = getbyID('btnofAll');
+let btnofInterview = getbyID('btnofInterview');
+let btnofRejected = getbyID('btnofRejected');
